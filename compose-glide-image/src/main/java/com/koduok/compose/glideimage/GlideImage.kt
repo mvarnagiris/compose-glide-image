3656729f5754a40b46c6567eff345337ac7e748e
Mantas Varnagiris <mantas.varnagiris@gmail.com>
2020-04-19 14:43:04 +0300

    Updated to dev09

--- compose-glide-image/src/main/java/com/koduok/compose/glideimage/GlideImage.kt
+++ compose-glide-image/src/main/java/com/koduok/compose/glideimage/GlideImage.kt
@@ -6,12 +6,13 @@ import androidx.compose.Composable
 import androidx.compose.onCommit
 import androidx.compose.state
 import androidx.ui.core.ContextAmbient
+import androidx.ui.core.Modifier
 import androidx.ui.core.WithConstraints
 import androidx.ui.foundation.Canvas
 import androidx.ui.foundation.Image
 import androidx.ui.graphics.ImageAsset
 import androidx.ui.graphics.asImageAsset
-import androidx.ui.layout.LayoutSize
+import androidx.ui.layout.fillMaxSize
 import androidx.ui.unit.IntPx
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestBuilder
@@ -25,19 +26,19 @@ fun GlideImage(
     customize: RequestBuilder<Bitmap>.() -> RequestBuilder<Bitmap> = { this }
 ) {
     WithConstraints { constraints, _ ->
-        var image by state<ImageAsset?> { null }
-        var drawable by state<Drawable?> { null }
+        val image = state<ImageAsset?> { null }
+        val drawable = state<Drawable?> { null }
         val context = ContextAmbient.current
 
         onCommit(model) {
             val target = object : CustomTarget<Bitmap>() {
                 override fun onLoadCleared(placeholder: Drawable?) {
-                    image = null
-                    drawable = placeholder
+                    image.value = null
+                    drawable.value = placeholder
                 }
 
                 override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                    image = resource.asImageAsset()
+                    image.value = resource.asImageAsset()
                 }
             }
 
@@ -64,18 +65,18 @@ fun GlideImage(
                 .into(target)
 
             onDispose {
-                image = null
-                drawable = null
+                image.value = null
+                drawable.value = null
                 glide.clear(target)
             }
         }
 
-        val theImage = image
-        val theDrawable = drawable
+        val theImage = image.value
+        val theDrawable = drawable.value
         if (theImage != null) {
-            Image(image = theImage)
+            Image(asset = theImage)
         } else if (theDrawable != null) {
-            Canvas(modifier = LayoutSize.Fill) { theDrawable.draw(nativeCanvas) }
+            Canvas(modifier = Modifier.fillMaxSize()) { theDrawable.draw(nativeCanvas) }
         }
     }
 }
