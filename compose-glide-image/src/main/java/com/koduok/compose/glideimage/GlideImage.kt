09283a4687bd1c2fc77d8e2379a26137c1df2d58
dgomolka <dgomolka@gmail.com>
2020-05-15 11:04:11 +0200

    - fixes incorrect disposal in AdapterList - added onImageReady callback - Compose updated to 0.1.0-dev11

--- compose-glide-image/src/main/java/com/koduok/compose/glideimage/GlideImage.kt
+++ compose-glide-image/src/main/java/com/koduok/compose/glideimage/GlideImage.kt
@@ -3,7 +3,7 @@ package com.koduok.compose.glideimage
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import androidx.compose.Composable
-import androidx.compose.onCommit
+import androidx.compose.onPreCommit
 import androidx.compose.state
 import androidx.ui.core.ContextAmbient
 import androidx.ui.core.Modifier
@@ -12,6 +12,7 @@ import androidx.ui.foundation.Canvas
 import androidx.ui.foundation.Image
 import androidx.ui.graphics.ImageAsset
 import androidx.ui.graphics.asImageAsset
+import androidx.ui.graphics.painter.drawCanvas
 import androidx.ui.layout.fillMaxSize
 import androidx.ui.unit.IntPx
 import com.bumptech.glide.Glide
@@ -19,55 +20,64 @@ import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
 import com.bumptech.glide.request.transition.Transition
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun GlideImage(
     model: Any,
+    onImageReady: (() -> Unit)? = null,
     customize: RequestBuilder<Bitmap>.() -> RequestBuilder<Bitmap> = { this }
 ) {
-    WithConstraints { constraints, _ ->
+    WithConstraints {
         val image = state<ImageAsset?> { null }
         val drawable = state<Drawable?> { null }
         val context = ContextAmbient.current
 
-        onCommit(model) {
-            val target = object : CustomTarget<Bitmap>() {
-                override fun onLoadCleared(placeholder: Drawable?) {
-                    image.value = null
-                    drawable.value = placeholder
-                }
-
-                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                    image.value = resource.asImageAsset()
-                }
-            }
-
-            val width =
-                if (constraints.maxWidth > IntPx.Zero && constraints.maxWidth < IntPx.Infinity) {
-                    constraints.maxWidth.value
-                } else {
-                    SIZE_ORIGINAL
-                }
-
-            val height =
-                if (constraints.maxHeight > IntPx.Zero && constraints.maxHeight < IntPx.Infinity) {
-                    constraints.maxHeight.value
-                } else {
-                    SIZE_ORIGINAL
-                }
-
+        onPreCommit(model) {
             val glide = Glide.with(context)
-            glide
-                .asBitmap()
-                .load(model)
-                .override(width, height)
-                .let(customize)
-                .into(target)
+            var target: CustomTarget<Bitmap>? = null
+            val job = CoroutineScope(Dispatchers.Main).launch {
+                target = object : CustomTarget<Bitmap>() {
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                        image.value = null
+                        drawable.value = placeholder
+                    }
+
+                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                        image.value = resource.asImageAsset()
+                        onImageReady?.invoke()
+                    }
+                }
+
+                val width =
+                    if (constraints.maxWidth > IntPx.Zero && constraints.maxWidth < IntPx.Infinity) {
+                        constraints.maxWidth.value
+                    } else {
+                        SIZE_ORIGINAL
+                    }
+
+                val height =
+                    if (constraints.maxHeight > IntPx.Zero && constraints.maxHeight < IntPx.Infinity) {
+                        constraints.maxHeight.value
+                    } else {
+                        SIZE_ORIGINAL
+                    }
+
+                glide
+                    .asBitmap()
+                    .load(model)
+                    .override(width, height)
+                    .let(customize)
+                    .into(target!!)
+            }
 
             onDispose {
                 image.value = null
                 drawable.value = null
                 glide.clear(target)
+                job.cancel()
             }
         }
 
@@ -76,7 +86,9 @@ fun GlideImage(
         if (theImage != null) {
             Image(asset = theImage)
         } else if (theDrawable != null) {
-            Canvas(modifier = Modifier.fillMaxSize()) { theDrawable.draw(nativeCanvas) }
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                drawCanvas { canvas, pxSize -> theDrawable.draw(canvas.nativeCanvas) }
+            }
         }
     }
 }
