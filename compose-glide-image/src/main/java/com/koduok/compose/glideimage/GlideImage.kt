43edc57dcf4333a1e56b1af3768639ea0054a78a
pawelsa <pawel.sapek97@gmail.com>
2020-11-05 12:51:01 +0100

    updated compose version and added all parameters for image composable

--- compose-glide-image/src/main/java/com/koduok/compose/glideimage/GlideImage.kt
+++ compose-glide-image/src/main/java/com/koduok/compose/glideimage/GlideImage.kt
@@ -9,13 +9,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.FrameManager
 import androidx.compose.runtime.onCommit
 import androidx.compose.runtime.stateFor
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.WithConstraints
-import androidx.compose.ui.graphics.ImageAsset
-import androidx.compose.ui.graphics.asImageAsset
-import androidx.compose.ui.graphics.drawscope.drawCanvas
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ContextAmbient
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestBuilder
@@ -29,12 +28,17 @@ import kotlinx.coroutines.launch
 @Composable
 fun GlideImage(
     model: Any,
+    modifier: Modifier,
+    contentScale: ContentScale = ContentScale.Fit,
+    alignment: Alignment = Alignment.Center,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null,
     onImageReady: (() -> Unit)? = null,
     customize: RequestBuilder<Bitmap>.() -> RequestBuilder<Bitmap> = { this }
 ) {
     WithConstraints {
-        val image = stateFor <ImageAsset?> (null) { null }
-        val drawable = stateFor<Drawable?> (null) { null }
+        val image = stateFor<ImageAsset?>(null) { null }
+        val drawable = stateFor<Drawable?>(null) { null }
         val context = ContextAmbient.current
 
         onCommit(model) {
@@ -47,7 +51,10 @@ fun GlideImage(
                         drawable.value = placeholder
                     }
 
-                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                    ) {
                         FrameManager.ensureStarted()
                         image.value = resource.asImageAsset()
                         onImageReady?.invoke()
@@ -87,9 +94,16 @@ fun GlideImage(
         val theImage = image.value
         val theDrawable = drawable.value
         if (theImage != null) {
-            Image(asset = theImage)
+            Image(
+                asset = theImage,
+                modifier = modifier,
+                contentScale = contentScale,
+                alignment = alignment,
+                alpha = alpha,
+                colorFilter = colorFilter
+            )
         } else if (theDrawable != null) {
-            Canvas(modifier = Modifier.fillMaxSize()) {
+            Canvas(modifier = Modifier.fillMaxSize().then(modifier)) {
                 drawIntoCanvas { theDrawable.draw(it.nativeCanvas) }
             }
         }
